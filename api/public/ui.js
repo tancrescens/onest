3d8f809bef443814349bde40c31254b3db718e41
Crescens <2e3crescens@gmail.com>
2024-05-10 16:50:47 +0800

    Getting PQList from database functionality update

--- api/public/ui.js
+++ api/public/ui.js
@@ -1,22 +1,38 @@
-function handleChange(event) {
-  console.log(event.target.value);
-  // to do: parse the value into URL in checkUEN
-}
-
-function isValidUEN(UEN, pqList) {}
-
-function checkUEN(event) {
+async function checkUEN(event) {
+  // todo: some checks, can be deleted later
   event.preventDefault();
   console.log("submit Button Pressed");
 
+  // Get the UENValue from the input field
+  const uenValue = document.getElementById("inputUEN").value;
+  // todo: some checks, can be deleted later
+  console.log("UEN Value:", uenValue); // Log the UEN value
+
+  // fetching this api route returns an array "jsonArray" containing all of the PQs
+  let jsonArray;
+  let pqArray;
   var url = `/api/pqs`;
-  fetch(url)
+  await fetch(url)
     .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
       return response.text();
     })
     .then((text) => {
-      const jsonArray = JSON.parse(text);
-      var isValid = isValidUEN(text, jsonArray);
+      jsonArray = JSON.parse(text);
+      // todo: some checks, can be deleted later
+      console.log("PQList inside scope: " + jsonArray);
     })
-    .catch();
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+      alert("Failed to load data. Please try again later");
+    });
+
+  // checking if the UEN contains a valid PQ.
+  // todo: some checks, can be deleted later
+  await console.log("PQList outside scope: " + jsonArray);
+  pqArray = await jsonArray.map((item) => item.pq);
+  // todo: some checks, can be deleted later
+  await console.log("pqArray outside scope: " + pqArray);
 }
