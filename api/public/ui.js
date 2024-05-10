e525b065cc35a02c12b9ef825358c917e4d8d61b
Crescens <2e3crescens@gmail.com>
2024-05-10 18:14:38 +0800

    included validation functionality

--- api/public/ui.js
+++ api/public/ui.js
@@ -4,11 +4,11 @@ async function checkUEN(event) {
   console.log("submit Button Pressed");
 
   // Get the UENValue from the input field
-  const uenValue = document.getElementById("inputUEN").value;
+  const uenValue = document.getElementById("inputUEN").value.toUpperCase();
   // todo: some checks, can be deleted later
   console.log("UEN Value:", uenValue); // Log the UEN value
 
-  // fetching this api route returns an array "jsonArray" containing all of the PQs
+  // fetching this api route returns an array "jsonArray" containing all of the PQs =====
   let jsonArray;
   let pqArray;
   var url = `/api/pqs`;
@@ -29,10 +29,47 @@ async function checkUEN(event) {
       alert("Failed to load data. Please try again later");
     });
 
-  // checking if the UEN contains a valid PQ.
+  // =========================================================================================
+  // checking if the UEN contains a valid PQ. ================================================
   // todo: some checks, can be deleted later
   await console.log("PQList outside scope: " + jsonArray);
   pqArray = await jsonArray.map((item) => item.pq);
   // todo: some checks, can be deleted later
   await console.log("pqArray outside scope: " + pqArray);
+  function validateInput(input) {
+    // 1. Ensure input only has 9-10 characters
+    // 2. Ensure input has no special characters
+    // 3. Ensure that first character is only digits or s or t, upper and lowercase accepted
+    // 4. Ensure last letter is always a letter
+    // 5. Ensure New UENs (starting with S&T), has valid PQ (check against PQ List from database/var pqArray)
+    // 1-4
+    let isValidFormat = /^[0-9stST][a-zA-Z0-9]{7,8}[a-zA-Z]$/.test(input);
+    if (!isValidFormat) {
+      alert(
+        "Input must be 9-10 characters long and the last character as a letter."
+      );
+      return;
+    }
+    // 5
+    // check if entered UEN is NEW UEN type: if not new UEN type, isValid ? else check against PQList
+    const isNewUEN = /^[stST]$/.test(uenValue.slice(0, 1));
+    // todo: some checks, can be deleted later
+    console.log("input's first character: " + uenValue.slice(0, 1));
+    console.log("Is new UEN?: " + isNewUEN);
+
+    if (isNewUEN === false) alert("Input is valid.");
+    if (isNewUEN) {
+      for (let i = 0; i < pqArray.length; i++) {
+        if (uenValue.slice(3, 5) == pqArray[i]) {
+          console.log(uenValue.slice(3, 5) + ":" + pqArray[i]);
+          alert("Input is valid.");
+          return;
+        }
+      }
+      alert("Input must contain a valid PQ.");
+      return;
+    }
+  }
+  await validateInput(uenValue);
+  // =========================================================================================
 }
