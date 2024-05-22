7409ac27a9004054f1dfdd7fbd8f73d45b024593
Crescens <2e3crescens@gmail.com>
2024-05-22 16:00:54 +0800

    fixed URI

--- api/index.js
+++ api/index.js
@@ -19,12 +19,14 @@ app.use(express.static("public"));
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
 mongoose
   .connect(process.env.URI)
   .then(() => {
     console.log("Connected to database!");
   })
   .catch(() => {
+    console.log(process.env.URI);
     console.log("Connection failed");
   });
 
